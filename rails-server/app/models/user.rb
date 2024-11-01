27e2b5340dc633fad462de5951ff18f820542218
Bishwo Dahal <bishwodahal123@gmail.com>
2024-11-01 14:25:15 -0500

    modified user to have unique email and username

--- rails-server/app/models/user.rb
+++ rails-server/app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   has_many :jobs
+  validates :username, presence: true, uniqueness: {case_sensitive: false}
+  validates :email, presence: true, uniqueness: { case_sensitive: false}
 end
--- rails-server/db/schema.rb
+++ rails-server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_29_140054) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_01_191618) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -56,6 +56,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_29_140054) do
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "jobs", "machines"
