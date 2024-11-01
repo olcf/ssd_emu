7f337a7b0a467e1f68ea3944af9e86c7de7528e0
Bishwo Dahal <bishwodahal123@gmail.com>
2024-11-01 12:03:39 -0500

    added seed values for database

--- rails-server/db/seeds.rb
+++ rails-server/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+User.create(first_name: "Bishwo", last_name: "Dahal", username: "bishwodahal", email: "someemail@gmail.com", password: "abcd")
+User.create(first_name: "John", last_name: "doe", username: "doejohn", email: "johndoe@gmail.com", password: "strong")
