1fbc00534590adfe183c4c3628124b4da82ff23a
Bishwo Dahal <bishwodahal123@gmail.com>
2024-11-01 16:58:46 -0500

    updated description of change on rename table columns

--- rails-server/db/migrate/20241101214739_renamed_errors_to_err_and_output_to_out.rb
+++ rails-server/db/migrate/20241101214739_renamed_errors_to_err_and_output_to_out.rb
@@ -1,3 +1,4 @@
+# Rails doesn't allow having errors as column name as it is used in many other places. There is hack to solve it but I wanted to change it to err (output for out just for uniformity) so that it won't be confusing about which error are we talking about 
 class RenamedErrorsToErrAndOutputToOut < ActiveRecord::Migration[7.0]
   def change
     rename_column :jobs, :output, :out
