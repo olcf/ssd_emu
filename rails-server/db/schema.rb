39d9b7d5ddf746c594257631d2d2a673deec0056
Bishwo Dahal <bishwodahal123@gmail.com>
2024-11-05 18:49:53 -0600

    migrated unique project name for project table

--- rails-server/db/schema.rb
+++ rails-server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_05_143221) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_06_004136) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,6 +53,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_05_143221) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_projects_on_name", unique: true
   end
 
   create_table "users", force: :cascade do |t|
