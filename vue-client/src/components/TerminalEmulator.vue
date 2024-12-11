1251ae69438b9923369241c256801ecf25de3409
Bishwo Dahal <bishwodahal123@gmail.com>
2024-12-11 13:58:41 -0600

    FRONTEND: terminal window can be maximized and minimized

--- vue-client/src/components/TerminalEmulator.vue
+++ vue-client/src/components/TerminalEmulator.vue
@@ -1,15 +1,37 @@
 <template>
-  <Terminal
-    welcome-message="Welcome to Emu(Supercomputer Emulation Project)"
-    prompt="username $"
-    style="font-family: 'Courier New', Courier, monospace; height: 100%"
-  ></Terminal>
+  <div>
+    <div
+      :class="{
+        'maximized-window': maximizedWindow,
+        'minimized-window': !maximizedWindow,
+      }"
+    >
+      <Button
+        class="max-min-icon"
+        icon="pi pi-window-maximize"
+        variant="text"
+        severity="contrast"
+        @click="maximizeMinimizeTerminal"
+      >
+      </Button>
+      <Terminal
+        welcome-message="Welcome to Emu(Supercomputer Emulation Project)"
+        style="height: 100%"
+        prompt="username $"
+      ></Terminal>
+    </div>
+  </div>
 </template>
 <script setup>
 import { api } from '@/apis'
 import Terminal from 'primevue/terminal'
 import TerminalService from 'primevue/terminalservice'
+import { ref } from 'vue'
+const maximizedWindow = ref(false)
 
+const maximizeMinimizeTerminal = function () {
+  maximizedWindow.value = !maximizedWindow.value
+}
 TerminalService.on('command', async command => {
   let commandList = command.split(' ')
   switch (commandList[0]) {
@@ -31,3 +53,26 @@ TerminalService.on('command', async command => {
   // TODO: Implement default methods and more commands after asking SSD.
 })
 </script>
+<style>
+.terminal-window {
+  font-family: 'Courier New', Courier, monospace;
+}
+.maximized-window {
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  height: 100vh;
+  width: 100vw;
+  z-index: 100;
+}
+.minimized-window {
+  position: relative;
+  height: 100vh;
+}
+.max-min-icon {
+  position: absolute;
+  right: 1rem;
+  top: 1rem;
+  cursor: pointer;
+}
+</style>
