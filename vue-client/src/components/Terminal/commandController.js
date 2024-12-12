a4b94c2d45632540dce968a46b99d6a901e32d7e
Bishwo Dahal <bishwodahal123@gmail.com>
2024-12-11 20:33:24 -0600

    implemented man and help command

--- vue-client/src/components/Terminal/commandController.js
+++ vue-client/src/components/Terminal/commandController.js
@@ -1,3 +1,12 @@
+/**
+ * All commands for command line are combined in this file. Every command should have following structure:
+ * {
+ * command: "Command Name",
+ * docs: "Explain what this feature can do",
+ * execute: ()=>{return String}
+ * }
+ */
+
 export const validCommands = [
   {
     command: 'info',
@@ -11,6 +20,37 @@ export const validCommands = [
     docs: 'ls works similar to other `ls` command on other applications',
     execute: command => {
       throw new Error(command + ' Feature on progress')
+    },
+  },
+  {
+    command: 'man',
+    docs: 'gives information for all commands',
+    execute: command => {
+      const commandList = command.split(' ')
+      if (commandList.length <= 1) {
+        // create a list of everything in this command except man
+        let manOutput = ''
+        validCommands.forEach(command => {
+          manOutput += command.command + '  --  ' + command.docs + '<br/>'
+        })
+        return manOutput
+      }
+      const askedCommandDocs = validCommands.findIndex(
+        obj => obj.command == commandList[1],
+      )
+      if (askedCommandDocs == -1) {
+        throw new Error(
+          `Couldn't find documentation of given command '${command}'`,
+        )
+      }
+      return validCommands[askedCommandDocs].docs
+    },
+  },
+  {
+    command: 'help',
+    docs: 'Use `man` or `man command` instead.',
+    execute: () => {
+      return 'Use `man` or `man command` instead.'
     },
   },
 ]
