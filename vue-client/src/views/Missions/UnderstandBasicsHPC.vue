1086f085479bd2cec2882eda063760e21d3fdb63
Bishwo Dahal <bishwodahal123@gmail.com>
2024-12-23 13:08:53 -0600

    FRONTEND: added demo pages for missions

---
+++ vue-client/src/views/Missions/UnderstandBasicsHPC.vue
@@ -0,0 +1,34 @@
+<template>
+  The HPC mission includes the Computational Sciences Building, part of the Oak
+  Ridge Leadership Computing Facility (OLCF), giving the world's most advanced
+  computational researchers an opportunity to tackler problems that would be
+  unthinkable on other systems. To date, OLCF has produced three supercomputers,
+  each bearing the title "world's fastest computer" in its time. The most recent
+  highly efficient pre-exascale system, Summit, reached a speed of 143.5
+  petaflops in FY 2018. At its theoretical peak, Summit is capable of 200
+  petaflops, eight times more performance than the previously top-ranked HPC
+  system, known as Titan. As supercomputing strategy continues to evolve, the
+  deign features championed at ORNL will ensure that DOE remains positioned at
+  the forefront of HPC on the international stage. Currently, infrastructure is
+  being installed to support the next supercomputer, Frontier, which is
+  scheduled for operations in FY 2022. The new Summit supercomputer is super in
+  more ways than just processing speed, thanks to the help of some engineers
+  from F&O's Modernization Project Office who participated on the design team.
+  Summit's systems have the ability to utilize moderate water temperature (70°F)
+  for cooling. Normally, water for ORNL's supercomputers needs to be chilled to
+  45°F. This primary design feature resulted in savings of approximately $3.7
+  million in construction costs by eliminating the need for a larger chilled
+  water plant. Other construction capital was eliminated by innovations in
+  electrical power configuration design for the servers resulting in an
+  estimated $500,000 in project savings. And best of all, for long-term
+  sustainable operations, Summit is designed to use medium temperature cooling
+  water for long-term utility cost savings and improved energy efficiency. Water
+  at temperatures of 64-70°F is provided direct to each computer rack. Water
+  returns at 85-100°F and is cooled by a tower water heat exchanger, and when
+  needed, a chilled water exchanger in series. Bypass of chilled water exchange
+  will be possible much of the year. This unique design will result an estimated
+  cost reduction of $1 million each year. Facility management is working with
+  the High-Performance Computing (HPC) operations staff to optimize cooling
+  system by trying computer run scheduler and computer diagnostic system to
+  mechanical controls to further reduce operational cost. Now that's SUPER!
+</template>
