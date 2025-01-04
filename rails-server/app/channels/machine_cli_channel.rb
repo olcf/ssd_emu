a7b2c2053e45c8a243863a23533f761da4ca7c99
Bishwo Dahal <bishwodahal123@gmail.com>
2025-01-03 23:43:31 -0600

    BACKEND: machineclichannel created to handle cli command requests

--- rails-server/app/channels/machine_cli_channel.rb
+++ rails-server/app/channels/machine_cli_channel.rb
@@ -1,8 +1,14 @@
 class MachineCliChannel < ApplicationCable::Channel
+  # TODO: update backend code to receive command and recognize command!
   def subscribed
     # stream_from "some_channel"
+    puts "Receiving message from subscribed!!!"
+    stream_from "MachineCliChannel"
   end
-
+  def execute_command(data)
+    given_command = data["cmd"]
+    ActionCable.server.broadcast("MachineCliChannel",message:"Response from backend")
+  end
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
