4473aea1e5e30ccc25e3c852796273c4ddc06dd1
Bishwo Dahal <bishwodahal123@gmail.com>
2025-01-16 16:04:30 -0600

    FRONTEND: added sample layout for mission section

--- vue-client/src/views/Missions/UnderstandBasicsHPC.vue
+++ vue-client/src/views/Missions/UnderstandBasicsHPC.vue
@@ -1,34 +1,53 @@
 <template>
-  The HPC mission includes the Computational Sciences Building, part of the Oak
-  Ridge Leadership Computing Facility (OLCF), giving the world's most advanced
-  computational researchers an opportunity to tackler problems that would be
-  unthinkable on other systems. To date, OLCF has produced three supercomputers,
-  each bearing the title "world's fastest computer" in its time. The most recent
-  highly efficient pre-exascale system, Summit, reached a speed of 143.5
-  petaflops in FY 2018. At its theoretical peak, Summit is capable of 200
-  petaflops, eight times more performance than the previously top-ranked HPC
-  system, known as Titan. As supercomputing strategy continues to evolve, the
-  deign features championed at ORNL will ensure that DOE remains positioned at
-  the forefront of HPC on the international stage. Currently, infrastructure is
-  being installed to support the next supercomputer, Frontier, which is
-  scheduled for operations in FY 2022. The new Summit supercomputer is super in
-  more ways than just processing speed, thanks to the help of some engineers
-  from F&O's Modernization Project Office who participated on the design team.
-  Summit's systems have the ability to utilize moderate water temperature (70°F)
-  for cooling. Normally, water for ORNL's supercomputers needs to be chilled to
-  45°F. This primary design feature resulted in savings of approximately $3.7
-  million in construction costs by eliminating the need for a larger chilled
-  water plant. Other construction capital was eliminated by innovations in
-  electrical power configuration design for the servers resulting in an
-  estimated $500,000 in project savings. And best of all, for long-term
-  sustainable operations, Summit is designed to use medium temperature cooling
-  water for long-term utility cost savings and improved energy efficiency. Water
-  at temperatures of 64-70°F is provided direct to each computer rack. Water
-  returns at 85-100°F and is cooled by a tower water heat exchanger, and when
-  needed, a chilled water exchanger in series. Bypass of chilled water exchange
-  will be possible much of the year. This unique design will result an estimated
-  cost reduction of $1 million each year. Facility management is working with
-  the High-Performance Computing (HPC) operations staff to optimize cooling
-  system by trying computer run scheduler and computer diagnostic system to
-  mechanical controls to further reduce operational cost. Now that's SUPER!
+  <div class="text-center text-3xl">Introduction to HPC</div>
+  <Accordion :value="['0']" multiple>
+    <AccordionPanel value="0">
+      <AccordionHeader>What is HPC?</AccordionHeader>
+      <AccordionContent>
+        <ul>
+          <li>Definition of HPC</li>
+          <li>Application of HPC</li>
+          <li>What it takes to make HPC</li>
+          <li>Top level overview of nodes</li>
+        </ul>
+      </AccordionContent>
+    </AccordionPanel>
+    <AccordionPanel value="1">
+      <AccordionHeader>HPC System Components</AccordionHeader>
+      <AccordionContent>
+        <ul>
+          <li>Each components described</li>
+          <li>How GPUs are interconnected</li>
+          <li>Storage systems</li>
+          <li>Filesystems and introduction of parallel file systems</li>
+        </ul>
+        <div class="text-xl pt-10">Answer this question!!!</div>
+        <div class="grid gap-4">
+          <div class="flex items-center gap-2">
+            <RadioButton inputId="answer1" name="question" value="Answer 1" />
+            <label for="answer1">Answer 1</label>
+          </div>
+          <div class="flex items-center gap-2">
+            <RadioButton inputId="answer2" name="question" value="Answer 2" />
+            <label for="answer2">Answer 2</label>
+          </div>
+          <div class="flex items-center gap-2">
+            <RadioButton inputId="answer3" name="question" value="Answer 3" />
+            <label for="answer3">Answer 3</label>
+          </div>
+          <div class="flex items-center gap-2">
+            <RadioButton inputId="answer4" name="question" value="Answer 4" />
+            <label for="answer4">Answer 4</label>
+          </div>
+        </div>
+      </AccordionContent>
+    </AccordionPanel>
+  </Accordion>
+
+  <Fieldset legend="Note!">
+    <p class="m-0">
+      This is a section for including any note to warn/provide note to user.
+    </p>
+  </Fieldset>
 </template>
+<script setup></script>
