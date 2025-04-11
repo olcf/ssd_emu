44e9ba7ac48ec889abcb0d2b21ff38ecda885a37
Bishwo Dahal <bishwodahal123@gmail.com>
2025-04-10 22:07:36 -0500

    BACKEND: create mission and chapter for each user when user is created

--- rails-server/app/models/user.rb
+++ rails-server/app/models/user.rb
@@ -1,5 +1,22 @@
 class User < ApplicationRecord
   has_many :jobs
+  has_many :user_missions
+  has_many :missions, through: :user_missions
+  has_many :user_chapters
+  has_many :chapters, through: :user_chapters
+
   validates :username, presence: true, uniqueness: {case_sensitive: false}
   validates :email, presence: true, uniqueness: { case_sensitive: false}
+  
+  after_create :create_mission_and_chapters_for_user
+  private
+  def create_mission_and_chapters_for_user
+    missions = Mission.all
+    missions.each do |mission|
+      UserMission.create!(user: self, mission: mission, completed: false)
+      mission.chapters.each do |chapter|
+        UserChapter.create!(user: self, chapter: chapter, completed: false)
+      end
+    end
+  end
 end
