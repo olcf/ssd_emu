4bf46eb702b7f3a05c7c9b03dc4115e1f0605bb2
Bishwo Dahal <bishwodahal123@gmail.com>
2025-06-22 12:25:04 -0500

    FRONTEND: user will login before anything loads

--- vue-client/src/App.vue
+++ vue-client/src/App.vue
@@ -1,42 +1,60 @@
 <script setup>
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
 import { RouterView } from 'vue-router'
 import LeftNavigationMenu from './components/LeftNavigation/LeftNavigationMenu.vue'
 import { useUserStore } from './stores/user'
 import TerminalEmulator from './components/Terminal/TerminalEmulator.vue'
+
 const userStore = useUserStore()
+const isLoggedIn = ref(false)
 
 onMounted(async () => {
+  console.log('App.vue: Starting user login...')
   await userStore.login()
+  console.log('App.vue: User login completed. User ID:', userStore.getUserId)
+  isLoggedIn.value = true
 })
 </script>
 
 <template>
-  <!-- Creating single Confirm Dialog so that it won't repeat for each call -->
-  <ConfirmDialog></ConfirmDialog>
-  <Toast></Toast>
+  <!-- Show loading until login is complete -->
+  <div v-if="!isLoggedIn" class="h-screen flex items-center justify-center bg-gray-900">
+    <div class="text-center">
+      <i class="pi pi-spin pi-spinner text-4xl text-blue-400 mb-4"></i>
+      <p class="text-gray-300">Loading...</p>
+      <p class="text-gray-300">Logging in User...</p>
 
-  <!-- Here we can create header -->
-  <Splitter layout="vertical" class="h-screen">
-    <SplitterPanel :size="96" :min-size="30">
-      <Splitter :gutter-size="3">
-        <SplitterPanel :size="15" :min-size="10">
-          <LeftNavigationMenu />
-        </SplitterPanel>
-        <SplitterPanel
-          :size="85"
-          :min-size="50"
-          class="p-2"
-          style="overflow-y: scroll"
-        >
-          <RouterView />
-        </SplitterPanel>
-      </Splitter>
-    </SplitterPanel>
-    <SplitterPanel :size="4" :min-size="3" class="p-0">
-      <TerminalEmulator></TerminalEmulator>
-    </SplitterPanel>
-  </Splitter>
+    </div>
+  </div>
+
+  <!-- Render app only after login is complete -->
+  <div v-else>
+    <!-- Creating single Confirm Dialog so that it won't repeat for each call -->
+    <ConfirmDialog></ConfirmDialog>
+    <Toast></Toast>
+
+    <!-- Here we can create header -->
+    <Splitter layout="vertical" class="h-screen">
+      <SplitterPanel :size="96" :min-size="30">
+        <Splitter :gutter-size="3">
+          <SplitterPanel :size="15" :min-size="10">
+            <LeftNavigationMenu />
+          </SplitterPanel>
+          <SplitterPanel
+            :size="85"
+            :min-size="50"
+            class="p-2"
+            style="overflow-y: scroll"
+          >
+            <RouterView />
+          </SplitterPanel>
+        </Splitter>
+      </SplitterPanel>
+      <SplitterPanel :size="4" :min-size="3" class="p-0">
+        <TerminalEmulator></TerminalEmulator>
+      </SplitterPanel>
+    </Splitter>
+  </div>
 
   <!-- Here we can create footer -->
 </template>
