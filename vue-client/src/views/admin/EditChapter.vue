dc7972c18037d047012b436b6e4e42b5b8be1a2a
Bishwo Dahal <6qd@ornl.gov>
2025-08-04 13:52:27 -0400

    FRONTEND: admin edit chapter added along with authentication for admin routes

---
+++ vue-client/src/views/admin/EditChapter.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import MilkdownEditor from '@/components/Editor/MilkdownEditor.vue';
+import { MilkdownProvider } from '@milkdown/vue';
+
+</script>
+
+<template>
+<MilkdownProvider>
+  <MilkdownEditor/>
+</MilkdownProvider>
+</template>
+
