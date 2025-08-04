c0d2e136be9b0389680017ae8c52133fa8846c0f
Bishwo Dahal <6qd@ornl.gov>
2025-08-04 15:16:52 -0400

    FRONTEND: viewer and markdown view for updating chapters markdown

--- vue-client/src/components/Editor/MilkdownEditor.vue
+++ vue-client/src/components/Editor/MilkdownEditor.vue
@@ -1,19 +1,40 @@
 <template>
-  <Milkdown />
+  <Splitter>
+    <SplitterPanel class="flex flex-col">
+      <span class="text-center p-1">Markdown Source</span>
+      <Milkdown/>
+    </SplitterPanel>
+    <SplitterPanel class="flex flex-col">
+      <span class="text-center p-1">Markdown Output</span>
+      <Textarea v-model="markdown" class="flex-1 resize-none" ></Textarea>
+    </SplitterPanel>
+  </Splitter>
+  <Button @click="extractMarkdown"> Extract Markdown</Button>
 </template>
 
 <script setup lang="ts">
 import { Milkdown, useEditor } from "@milkdown/vue";
 import { Crepe } from "@milkdown/crepe";
+import { ref } from "vue";
 
-const markdown = `This is your content!`;
+let markdown = ref('');
+const crepe = ref(null)
 
 useEditor((root) => {
-  const crepe = new Crepe({
+  const c = new Crepe({
     root,
-    defaultValue: markdown,
+    defaultValue: 'Default Content',
   });
+  crepe.value = c
 
-  return crepe;
+  return c;
 })
+
+
+
+async function extractMarkdown(){
+  markdown.value = await crepe.value.getMarkdown();
+}
+
+
 </script>
