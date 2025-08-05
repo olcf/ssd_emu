43fdd5f138518a365e15f25bf324596e5d205d7a
Bishwo Dahal <6qd@ornl.gov>
2025-08-05 11:37:47 -0400

    FRONTEND: Implemented RSA token to change every 30 seconds

---
+++ vue-client/src/components/ToolsSet/ToolsSet.vue
@@ -0,0 +1,92 @@
+<template>
+  <div
+    ref="dragWrapper"
+    class="fixed left-3 z-50 cursor-move"
+    :style="{ top: posY + 'px' }"
+    @mousedown="startDrag"
+    @touchstart="startDrag"
+  >
+    <div
+      class=" flex overflow-hidden bg-gray-900 rounded-full px-2 py-2 shadow-lg"
+    >
+      <Button
+        v-for="btn in buttons"
+        :key="btn.id"
+        :label="btn.label"
+        :title="btn.title"
+        @click="btn.show = !btn.show"
+        style="width: 50px; height: 50px; border-radius: 100%;"
+      />
+    </div>
+
+    <div class="flex flex-col space-y-4 mt-4">
+      <template v-for="btn in buttons" :key="btn.id + '-component'">
+        <component v-if="btn.show" :is="btn.component" />
+      </template>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { reactive, ref, onMounted, onBeforeUnmount } from 'vue'
+import RsaToken from './RsaToken.vue'
+
+const buttons = reactive([
+  { id: 'rsa', label: 'RSA', show: false, title: 'Toggle RSA Token', component: RsaToken },
+])
+
+const posY = ref(window.innerHeight - 150) // initial vertical position near bottom
+const dragWrapper = ref(null)
+
+let dragging = false
+let startY = 0
+let startPosY = 0
+
+// Found this online on a repo
+function onMouseMove(e) {
+  if (!dragging) return
+  // If we want to make it compatible for ipad or touch devices, this wont work
+  const clientY =  e.clientY
+
+  let newY = startPosY + (clientY - startY)
+
+  const maxY = window.innerHeight - (dragWrapper.value?.offsetHeight || 100)
+  newY = Math.min(Math.max(0, newY), maxY)
+
+  posY.value = newY
+}
+
+function onMouseUp() {
+  dragging = false
+  window.removeEventListener('mousemove', onMouseMove)
+  window.removeEventListener('touchmove', onMouseMove)
+  window.removeEventListener('mouseup', onMouseUp)
+  window.removeEventListener('touchend', onMouseUp)
+}
+
+function startDrag(e) {
+  e.preventDefault()
+  dragging = true
+  startY = e.clientY
+  startPosY = posY.value
+
+  window.addEventListener('mousemove', onMouseMove)
+  window.addEventListener('touchmove', onMouseMove, { passive: false })
+  window.addEventListener('mouseup', onMouseUp)
+  window.addEventListener('touchend', onMouseUp)
+}
+
+onMounted(() => {
+  window.addEventListener('resize', () => {
+    const maxY = window.innerHeight - (dragWrapper.value?.offsetHeight || 100)
+    posY.value = Math.min(posY.value, maxY)
+  })
+})
+
+onBeforeUnmount(() => {
+  window.removeEventListener('mousemove', onMouseMove)
+  window.removeEventListener('touchmove', onMouseMove)
+  window.removeEventListener('mouseup', onMouseUp)
+  window.removeEventListener('touchend', onMouseUp)
+})
+</script>
